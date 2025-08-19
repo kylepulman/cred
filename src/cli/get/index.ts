63c626e0b978cc16fae50b6075b14d4436203870
Kyle P. Ulman <kylepulman@proton.me>
2025-08-19 14:14:08 -0400

    feat: --copy

--- src/cli/get/index.ts
+++ src/cli/get/index.ts
@@ -3,16 +3,26 @@ import { z } from 'zod'
 import action from '../lib/action'
 import { selectAll, selectById } from '@/models/cred'
 import { decrypt } from '@/lib/encryption'
+import clipboardy from 'clipboardy'
 
 const Input = z.object({
-  id: z.string().optional()
+  id: z.string().optional(),
+  copy: z.union([
+    z.literal('id'),
+    z.literal('name'),
+    z.literal('identifier'),
+    z.literal('secret'),
+    z.literal(true),
+    z.undefined()
+  ])
 })
 
 type Input = z.infer<typeof Input>
 
 function newInput() {
   return {
-    id: ''
+    id: '',
+    copy: undefined
   } satisfies Input
 }
 
@@ -44,6 +54,7 @@ type SelectAllOutput = z.infer<typeof SelectAllOutput>
 export default new Command()
   .name('get')
   .description('Get credential by ID or list credentials.')
+  .option('--copy [field]')
   .arguments("[id]")
   .action((...args) => action<Input>(
     args,
@@ -59,6 +70,16 @@ export default new Command()
         const output: SelectByIdOutput = {
           ...result,
           secret: (await decrypt(process.env.CRED_PASS, result.secret)).toString()
+        }
+
+        if (input.copy) {
+          if (input.copy === true) {
+            await clipboardy.write(output.secret)
+            console.log('secret copied!')
+          } else {
+            await clipboardy.write(output[input.copy])
+            console.log(`${input.copy} copied!`)
+          }
         }
 
         console.log(output)
